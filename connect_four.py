58c4393efd9b989160a1c576eb2834792d3c0b53
Robert <roberthagemann@live.com>
2019-04-24 20:54:53 +0200

    Abstracting methods for getting a field, validating user input, getting open fields

--- connect_four.py
+++ connect_four.py
@@ -7,7 +7,8 @@ logger = logging.getLogger('cf')
 class ConnectFour:
     def __init__(self, player1, player2):
         self.players = {1: player1, 2: player2}
-        self.currentTurn = 1
+        self.symbols = {1: 'X', 2: 'O'}
+        self.current_turn = 1
         self.fields = []
         self.rows = [6, 5, 4, 3, 2, 1]
         self.cols = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
@@ -20,19 +21,57 @@ class ConnectFour:
 
     def start(self):
         counter = 0
-        while counter < 5:
+        while counter < 3:
+            current_player = self.current_turn
             self.print_board()
-            counter += 1
-            position = input('Place your next token: ')
-            self.place_token(position)
+            while current_player == self.current_turn:
+                position = input(self.players[self.current_turn] + ', place your next token: ')
+                try:
+                    self.place_token(position)
+                    self.switch_current_player()
+                    counter += 1
+                except ValueError as err:
+                    print(getattr(err, 'message', str(err)))
 
     def place_token(self, position):
-        col = position[:1]
-        row = position[1:]
+        position_obj = self.convert_position(position)
+        field = self.get_field(position_obj['col'], position_obj['row'])
+        self.validate_position(position_obj['col'], position_obj['row'])
+        field.state = self.symbols[self.current_turn]
 
+    def convert_position(self, position):
+        try:
+            col = position[0:1].upper()
+            row = int(position[1:2])
+            return {'col': col, 'row': row}
+        except ValueError:
+            raise ValueError('Position ' + position + ' does not exist.')
+
+    def get_field(self, col, row):
         for field in self.fields:
-            if field.col == col and field.row == int(row):
-                field.state = 'X'
+            if str(field.col) == col and int(field.row) == row:
+                return field
+        raise ValueError('Position ' + col + str(row) + ' does not exist.')
+
+    def get_open_fields(self):
+        fields = []
+        # Filter blocked fields
+        for field in self.fields:
+            if field.state == ' ':
+                fields.append(field)
+        for col in self.cols:
+            col_fields = list(filter(lambda e: e.col == col, fields))
+            sorted_col_fields = sorted(col_fields, key=lambda x: x.row)
+            fields.append(sorted_col_fields[0])
+        return fields
+
+    def validate_position(self, col, row):
+        open_fields = self.get_open_fields()
+
+        for field in open_fields:
+            if str(field.col) == col and int(field.row) == row and field.state == ' ':
+                return field
+        raise ValueError('Position ' + col + str(row) + ' is already taken. Please choose another position.')
 
     def print_board(self):
         print('')
@@ -49,6 +88,9 @@ class ConnectFour:
             print(col, end=' ')
 
         print('')
+
+    def switch_current_player(self):
+        self.current_turn = 2 if self.current_turn == 1 else 1
 
 
 class Field:
